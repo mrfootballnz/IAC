613177ee39657310652f17f808545530de3b7ef6
mrfootballnz <75594948+mrfootballnz@users.noreply.github.com>
2024-11-13 15:07:41 +1300

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,13 @@
+//Variables for vsphere
+variable "vsphere_server" {
+  default = ""
+}
+variable "vsphere_user" {
+    default=""
+}
+variable "vsphere_password" {
+  default = ""
+}
+variable "vsphere_virtual_machine-name" {
+  type = string
+}
